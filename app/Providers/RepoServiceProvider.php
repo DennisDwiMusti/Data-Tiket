099b101060b706a4036162b304b37c02f4abb550
DennisDwiMusti <dennisdwi18@gmail.com>
2025-03-20 09:34:41 +0700

    perf: menambahkan repository satsiun dan tiket pada povider

--- app/Providers/RepoServiceProvider.php
+++ app/Providers/RepoServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repository\TiketRepository;
+use App\Repository\TiketImplement;
+use App\Repository\StasiunRepository;
+use App\Repository\StasiunImplement;
 
 class RepoServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,8 @@ class RepoServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TiketRepository::class, TiketImplement::class);
+        $this->app->bind(StasiunRepository::class, StasiunImplement::class);
     }
 
     /**
