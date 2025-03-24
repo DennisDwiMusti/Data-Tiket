a03a44d1fb1cb4080e6b153f00643471b21fffde
DennisDwiMusti <dennisdwi18@gmail.com>
2025-03-25 05:44:24 +0700

    refactor: perubahan penamaan

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,6 @@ use App\Http\Controllers\StasiunController;
 Route::get('/', function () {
     return view('home');
 });
-Route::prefix('tiket')->name('tiket.')->group(function() {
-    Route::get('/', [TiketController::class, 'index'])->name('index');
-    Route::get('/tambah', [TiketController::class, 'create'])->name('create');
-    Route::post('/store', [TiketController::class, 'store'])->name('store');
-    Route::get('/{id}', [TiketController::class, 'edit'])->name('edit');
-    Route::patch('/{id}', [TiketController::class, 'update'])->name('update');
-    Route::delete('/{id}', [TiketController::class, 'destroy'])->name('destroy');
-});
-    
 Route::prefix('stasiun')->name('stasiun.')->group(function () {
     Route::get('/', [StasiunController::class, 'index'])->name('index');
     Route::get('/tambah', [StasiunController::class, 'create'])->name('create');
@@ -24,3 +15,13 @@ Route::prefix('stasiun')->name('stasiun.')->group(function () {
     Route::patch('/{id}', [StasiunController::class, 'update'])->name('update');
     Route::delete('/{id}', [StasiunController::class, 'destroy'])->name('destroy');
 });
+
+Route::prefix('tiket')->name('tiket.')->group(function () {
+    Route::get('/', [TiketController::class, 'index'])->name('index');
+    Route::get('/tambah', [TiketController::class, 'create'])->name('create');
+    Route::post('/store', [TiketController::class, 'store'])->name('store');
+    Route::get('/{id}', [TiketController::class, 'edit'])->name('edit');
+    Route::patch('/{id}', [TiketController::class, 'update'])->name('update');
+    Route::delete('/{id}', [TiketController::class, 'destroy'])->name('destroy');
+});
+
