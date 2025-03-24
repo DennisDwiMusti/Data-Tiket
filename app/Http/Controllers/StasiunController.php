8cf4d91382d6cadc1c351f9750cfc99868a0ab5d
DennisDwiMusti <dennisdwi18@gmail.com>
2025-03-25 05:34:02 +0700

    fix: perbaikan  error karena tidak terhubung ke repository

--- app/Http/Controllers/StasiunController.php
+++ app/Http/Controllers/StasiunController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Stasiun;
 use Illuminate\Http\Request;
+use App\Repository\StasiunRepository;
 
 class StasiunController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index( )
     {
         //
+        $stasiuns = Stasiun::all();
+        return view('stasiun.index', compact('stasiuns'));
     }
 
     /**
@@ -21,6 +25,15 @@ class StasiunController extends Controller
     public function create()
     {
         //
+        return view('stasiun.create');
+    }
+
+    protected $stasiunRepository
+;
+
+    public function __construct(StasiunRepository $repository)
+    {
+        $this->stasiunRepository = $repository;
     }
 
     /**
@@ -29,12 +42,14 @@ class StasiunController extends Controller
     public function store(Request $request)
     {
         //
+        $this->stasiunRepository->store($request->all());
+        return redirect()->route('stasiun.index')->with('success', 'Data stasiun berhasil ditambahkan');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Stasiun $stasiun)
+    public function show(string $id)
     {
         //
     }
@@ -42,24 +57,35 @@ class StasiunController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Stasiun $stasiun)
+    public function edit(string $id)
     {
         //
+        $stasiun = Stasiun::findOrFail($id);
+        return view('stasiun.edit', compact('stasiun'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Stasiun $stasiun)
+    public function update(Request $request, string $id)
     {
         //
+        $this->stasiunRepository->update($request->all(), $id);
+        return redirect()->route('stasiun.index')->with('success', 'Data stasiun berhasil diubah');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Stasiun $stasiun)
+    public function destroy(string $id)
     {
-        //
+        $stasiun = Stasiun::find($id);
+
+        if (!$stasiun) {
+            return redirect()->route('stasiun.index')->with('deleted', 'Data stasiun tidak ditemukan');
+        }
+
+        $this->stasiunRepository->destroy($id);
+        return redirect()->route('stasiun.index')->with('deleted', 'Data stasiun berhasil dihapus');
     }
 }
